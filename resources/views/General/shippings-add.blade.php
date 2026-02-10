05db770569c1026bfe456fbbe46eb2d91a5e68ab
DevReka-indo <ti.dev.reka@gmail.com>
2026-02-10 14:40:33 +0700

    Add reference date input on shipping add and edit forms

--- resources/views/General/shippings-add.blade.php
+++ resources/views/General/shippings-add.blade.php
@@ -77,6 +77,18 @@
               @enderror
             </div>
             <div class="col-md-6">
+              <label class="form-label">Tanggal Ref</label>
+              <input
+                type="date"
+                name="referenceDate"
+                value="{{ old('referenceDate') }}"
+                class="form-control @error('referenceDate') is-invalid @enderror"
+              />
+              @error('referenceDate')
+                <div class="invalid-feedback">{{ $message }}</div>
+              @enderror
+            </div>
+            <div class="col-md-6">
               <label class="form-label">Proyek <span class="text-danger">*</span></label>
               <input
                 type="text"
